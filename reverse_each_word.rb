a0c7c02e1890246754443b531161b288d8da59f6
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-26 21:50:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,4 @@
 def reverse_each_word(sentence1)
-    words_array = sentence1.split(" ")
-  new_array = []
-  new_array = words_array.collect {|word| word.reverse }
-  new_array.join(" ")
+   
 end
   
